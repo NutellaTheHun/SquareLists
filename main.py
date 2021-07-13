8621e39b1e14bde68c0cc43fc72000f1bcc70176
NutellaTheHun <nbrilmayer3@gmail.com>
2021-07-12 22:46:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ df = pd.read_csv(inputfilename, parse_dates=['Pickup Date'])
 FormatProductOptions(df)
 #formats the variation 6 cookies/scones and builds into individual lines
 df = CookieBuilder(df)
-
 #"Full Name" column and "Order Type" are also used in Frontlist.py and Forecast.py, and the column renames
 df["Full Name"] = df["Shipping First Name"] + ' ' + df["Shipping Last Name"]
 df["Order Type"] = np.where(pd.isnull(df["Shipping Address"]), '', np.where(df["Shipping Address"] == "285 Beacon St" , "Pickup", "Delivery"))
